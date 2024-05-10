2fd9266685cecbafa8d4965dea8ae378306f8c50
César García <cesar.garciamcs@outlook.com>
2024-05-10 00:02:33 -0600

    Agregando imagenes de la galeria

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-alert('hola mundo')
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria();
+})
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i = 1; i <= 16; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = 'Imagen Galeria';
+        galeria.appendChild(imagen);
+    }
+}
