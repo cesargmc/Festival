7c50649368d89def3806fb10dae10e37525a8101
César García <cesar.garciamcs@outlook.com>
2024-05-10 00:21:00 -0600

    Agregando SASS a la galeria

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function crearGaleria() {
+    const galeria_imagenes = 16
     const galeria = document.querySelector('.galeria-imagenes');
 
-    for(let i = 1; i <= 16; i++) {
+    for(let i = 1; i <= galeria_imagenes; i++) {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = 'Imagen Galeria';
