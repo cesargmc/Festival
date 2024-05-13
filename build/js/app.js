d95fcd4d3741f0205fed50bfe1686d69d5daf67b
César García <cesar.garciamcs@outlook.com>
2024-05-13 00:22:55 -0600

    Agregando estilo a los enlaces

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     navegacionFija();
     crearGaleria();
+    resaltarEnlace();
 })
 
 function navegacionFija() {
@@ -68,4 +69,28 @@ function cerrarModal() {
 
         modal?.remove();
     }, 500);
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active');
+            }
+        })
+    })
 }
