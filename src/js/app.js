93a4e43b4a2e7c701a1d13c6988af296fbe480d7
César García <cesar.garciamcs@outlook.com>
2024-05-16 18:17:50 -0600

    Agregando las imagenes pequeñas en la galeria

--- src/js/app.js
+++ src/js/app.js
@@ -27,9 +27,9 @@ function crearGaleria() {
         imagen.loading = 'lazy';
         imagen.width = '300';
         imagen.height = '200';
-        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.src = `src/img/gallery/thumb/${i}.jpg`;
         imagen.alt = 'Imagen Galeria';
-        
+
         // Event Handler
         imagen.onclick = function() {
             mostarImagen(i);
