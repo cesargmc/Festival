de356da5571dba5c94582f4d6015187d07e18e5d
César García <cesar.garciamcs@outlook.com>
2024-05-16 22:58:23 -0600

    Utilizando imagenes en formato AVIF, Webp o Jpg

--- src/js/app.js
+++ src/js/app.js
@@ -23,12 +23,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
     for(let i = 1; i <= galeria_imagenes; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy';
-        imagen.width = '300';
-        imagen.height = '200';
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`;
-        imagen.alt = 'Imagen Galeria';
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+        `;
 
         // Event Handler
         imagen.onclick = function() {
@@ -40,9 +40,12 @@ function crearGaleria() {
 }
 
 function mostarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = 'Imagen Galeria';
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
     // Modal
     const modal = document.createElement('DIV');
